11547c504655f76ff8e405f6cc0f040ff843eb96
Adam <adamj500@interia.pl>
2019-10-13 18:29:23 +0200

    Freezeball and Fireball

--- Assets/Scripts/ProjectileMov.cs
+++ Assets/Scripts/ProjectileMov.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ProjectileMov : MonoBehaviour
 {
+    public float projectileSpeed;
+    private float speed = 0.01f;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity = transform.right * projectileSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*rb.AddForce(transform.right * speed);
+        if(speed < projectileSpeed)
+            speed *= Time.deltaTime;*/
+    }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+            Destroy(gameObject);
     }
 }
